b05088680fd42393cdeb50595920824eb4a3f88c
SiderealArt <nelson22768384@gmail.com>
2019-01-07 19:35:32 +0800

    Update clock.js

--- JS/clock.js
+++ JS/clock.js
@@ -35,8 +35,5 @@ function black() {
   }, false);
 
   window.addEventListener("load",function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
+setTimeout(window.scrollTo(0,1),100);
 });
